623a8de24985246cee559618198b750d6ba8ee8b
Nguyen Thanh Trung <nguyen.thangtrung0809@gmail.com>
2026-04-28 03:03:15 +0700

    fix-wwwroot

--- AppCoTuong/CoTuongAPI/obj/Debug/net10.0/CoTuongAPI.AssemblyInfo.cs
+++ AppCoTuong/CoTuongAPI/obj/Debug/net10.0/CoTuongAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoTuongAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f45bdce73cabb3548dd2718ecef29ee3275d498")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+111e6bb1dd762935e16cc63c980ca583cb73967d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoTuongAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoTuongAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
